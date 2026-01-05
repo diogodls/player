d5aa530e1b69c045206f18a05d27ba32844b846a
didi <diogoddlsr@gmail.com>
2026-01-05 17:01:03 -0300

    deps(#1): adiciona font awesome ao projeto

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,17 +1,31 @@
 import styles from "./Navbar.module.scss";
+import reactLogo from '../../../assets/react.svg'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChartLine, faPeopleGroup, faUser} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
 
     return (
         <nav className={styles.navbar}>
-             <img src={"../../assets/react.svg"} />
+          <img src={reactLogo}  alt={""}/>
 
 
-             <div className={styles.buttons}>
-                <span>botao1</span>
-                <span>botao2</span>
-                <span>botao3</span>
-             </div>
+          <div className={styles.buttons}>
+            <button className={styles.button}>
+              <FontAwesomeIcon icon={faChartLine}/>
+              <span>Tela do treinador</span>
+            </button>
+
+            <button className={styles.button}>
+              <FontAwesomeIcon icon={faUser}/>
+              <span>Analise individual</span>
+            </button>
+
+            <button className={styles.button}>
+              <FontAwesomeIcon icon={faPeopleGroup}/>
+              <span>Análise de equipe</span>
+            </button>
+          </div>
         </nav>
     );
 };
