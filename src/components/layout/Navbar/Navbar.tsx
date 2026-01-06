15753fdfdc9b32f3b41d55d4f5117f5a2947b0ff
jalobler7 <jablobler@inf.ufsm.br>
2026-01-06 10:33:53 -0300

    feat(#2): cria activeButton

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -2,15 +2,21 @@ import styles from "./Navbar.module.scss";
 import reactLogo from '../../../assets/react.svg'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faChartLine, faPeopleGroup, faUser,faHouse} from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
 
     return (
         <nav className={styles.navbar}>
           <img src={reactLogo}  alt={""}/>
 
           <div className={styles.buttons}>
-            <button className={styles.button}>
+            <button
+              className={active ? styles.active : styles.button}
+              onClick={() => setActive(!active)}
+            >
               <FontAwesomeIcon icon={faHouse}/>
               <span>Home</span>
             </button>
