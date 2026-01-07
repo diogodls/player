0ac6ec9cb506bc6a802ba92031260a18672bc440
didi <diogoddlsr@gmail.com>
2026-01-07 10:18:15 -0300

    feat(#3): cria componente de descritive text

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/layout/Navbar/Navbar.tsx'
+import DescritiveText from "./components/DescritiveText/DescritiveText.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <Navbar />
+      <Navbar />
+      <DescritiveText />
     </>
   )
 }
---
+++ src/components/DescritiveText/DescritiveText.tsx
@@ -0,0 +1,39 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChartColumn} from "@fortawesome/free-solid-svg-icons";
+import {classNames} from "../../utils/classNames.ts";
+import styles from './DescritiveText.module.scss';
+import {useNavigate} from "react-router";
+
+const DescritiveText = () => {
+  const navigate = useNavigate();
+  return (
+    <div className={styles.content}>
+      <div className={styles.texts}>
+        <span className={styles.icon}><FontAwesomeIcon icon={faChartColumn}/></span>
+        <span>
+          <h1 className={styles.title}>Plataforma de análise de</h1>
+          <h1 className={styles.secondaryTitle}>Performance esportiva</h1>
+        </span>
+
+        <span className={styles.informativeText}>Software de análise esportiva, feito para técnicos e analistas capturarem dados sobre seus atletas.</span>
+      </div>
+
+      <div className={styles.linkButtons}>
+        <button
+          className={classNames([styles.button, styles.actionButton])}
+          onClick={() => navigate('/coach')}
+          >
+          Tela do treinador
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => navigate('/analise')}
+        >
+          Começar análise
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DescritiveText;
