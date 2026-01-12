5d4b15907dd5bbc81594b6d0fdf318bfe1764630
guzttx <gugamachado98@hotmail.com>
2026-01-12 09:35:42 -0300

    feat #4 - ajeita background

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import DescritiveText from "../../components/DescritiveText/DescritiveText.tsx";
+import styles from './Home.module.scss';
 
-const Home = () => {
-    return (
-      <div>
-          <DescritiveText />
-      </div>
-    );
+export default function Home() {
+  return (
+    <div className={styles.container}>
+  <div className={styles.content}>
+    <DescritiveText />
+  </div>
+</div>
+
+  );
 }
-
-export default Home;
