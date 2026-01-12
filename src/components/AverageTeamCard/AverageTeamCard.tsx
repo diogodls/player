28e73cee89346619c6de77e4d571a8586cee9937
jalobler7 <jablobler@inf.ufsm.br>
2026-01-12 11:46:24 -0300

    feat(#10)-card-media

--- src/components/AverageTeamCard/AverageTeamCard.tsx
+++ src/components/AverageTeamCard/AverageTeamCard.tsx
@@ -6,21 +6,21 @@ import type {Icon} from "@fortawesome/fontawesome-svg-core";
 
 const cards = [
   {
-    name:"Média ofensiva geral",
-    color:"#2563eb",
-    value:"10",
+    name:"Média Ofensiva Geral",
+    color:"linear-gradient(135deg, #2563eb, #1e40af)",
+    value:"10.2",
     icon: faChartColumn
   },
   {
-    name:"Média defensiva geral",
-    color: "#f97316",
-    value: "20",
+    name:"Média Defensiva Geral",
+    color: "linear-gradient(135deg, #f97316, #c2410c)",
+    value: "20.5",
     icon: faChartColumn
   },
   {
-    name:"Média defensiva geral",
-    color:"#22c55e",
-    value: "10",
+    name:"Média Geral da Equipe",
+    color:"linear-gradient(135deg, #22c55e, #15803d)",
+    value: "10.3",
     icon:faChartColumn
   }
 ]
@@ -38,11 +38,15 @@ const AverageTeamCard =  () => {
       {cards.map((card, index) => (
         <div className={styles.card}
           key={index}
-          style={{backgroundColor: card.color}}
+          style={{background: card.color}}
         >
-          <FontAwesomeIcon icon={card.icon}/>
-            <span>{card.name}</span>
-            <strong> {card.value}</strong>
+          <div className={styles.left}>
+             <span className={styles.cardName}>{card.name}</span>
+             <span className={styles.cardValue}>{card.value}</span>
+          </div>
+          <div className={styles.right}>
+            <FontAwesomeIcon icon={card.icon}/>
+          </div>
         </div>
       ))}
     </div>
