5730972c383b16a9919c28d17a7c31d18b1edb57
guzttx <gugamachado98@hotmail.com>
2026-01-12 10:10:59 -0300

    fix #4 ajeita a home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import DescritiveText from "../../components/DescritiveText/DescritiveText.tsx";
 import styles from './Home.module.scss';
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.container}>
   <div className={styles.content}>
@@ -11,3 +11,5 @@ export default function Home() {
 
   );
 }
+
+export default Home;
