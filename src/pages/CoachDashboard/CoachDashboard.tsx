fefec4c8ffb2874c98ceea9d96fb94519b994c31
jalobler7 <jablobler@inf.ufsm.br>
2026-01-13 10:16:02 -0300

    feat(#10)-card-media: carrega os dados do mock

--- src/pages/CoachDashboard/CoachDashboard.tsx
+++ src/pages/CoachDashboard/CoachDashboard.tsx
@@ -1,13 +1,15 @@
-import AverageTeamCard from "../../components/AverageTeamCard/AverageTeamCard.tsx";
 import styles from "./CoachDashboard.module.scss";
+import {useApi} from "../../hooks/useApi.ts";
+import type {CoachDashboard} from "./index";
+import AverageTeamCard from "../../components/AverageTeamCard/AverageTeamCard.tsx";
 
 const CoachDashboard = () => {
+  const { data } = useApi<CoachDashboard>("coach-dashboard");
+
   return (
     <div className={styles.container}>
-      <AverageTeamCard/>
+      <AverageTeamCard cards={data?.averageTeamCards ?? []}/>
     </div>
   );
 }
 export default CoachDashboard;
-
-
