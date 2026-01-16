e01d455013b76d2b6661fb6f7f4510f3fa94495a
jalobler7 <jablobler@inf.ufsm.br>
2026-01-16 10:28:23 -0300

    feat(#11): finaliza header dashboard

--- src/components/HeaderDashboard/HeaderDashboard.tsx
+++ src/components/HeaderDashboard/HeaderDashboard.tsx
@@ -1,39 +1,41 @@
 import {faCodeCompare, faPeopleGroup, faUser, faFileExport} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import styles from "./HeaderDashboard.module.scss"
-import { useState } from "react";
-import {useNavigate} from "react-router";
 import {classNames} from "../../utils/classNames.ts";
+import type {IconDefinition} from "@fortawesome/fontawesome-svg-core";
 
-const buttons= [
+type ButtonsType = {
+  label: string,
+  icon: IconDefinition,
+  mode: 'team' | 'individual' | 'compare'
+}
+
+const buttons: ButtonsType[] = [
   {
     label: "Análise da Equipe",
     icon: faPeopleGroup,
-    link:''
+    mode:'team'
   },
   {
     label: "Análise Individual",
     icon: faUser,
-    link:''
+    mode:'individual'
   },
   {
     label: "Compare",
     icon: faCodeCompare,
-    link:''
+    mode:'compare'
   }
 ]
 
-const HeaderDashboard = () => {
+type Props = {
+  viewMode: 'team' | 'individual' | 'compare';
+  onChangeView: (mode: Props['viewMode']) => void;
+};
 
-  const [activeIndex, setActiveIndex] = useState(0);
-  const navigate = useNavigate();
+const HeaderDashboard = ({ viewMode, onChangeView}: Props) => {
 
-  const buttonAction = (index:number, link: string) => {
-    setActiveIndex(index);
-    navigate(link);
-  };
-
-  return(
+  return (
     <div className={styles.div}>
       <div>
         <span className={styles.text}>Dashboard de Performance da Equipe</span>
@@ -41,18 +43,18 @@ const HeaderDashboard = () => {
       </div>
       <div className={styles.buttonsFlex}>
         <div className={styles.button}>
-          {buttons.map((button, index) => (
+          {buttons.map((button) => (
             <button
-              key={button.label}
+              key={button.mode}
               className={classNames([
                 styles.buttons,
-                index === activeIndex
-                  ? button.label === "Compare"
+                viewMode === button.mode
+                  ? button.mode === 'compare'
                     ? styles.activePurple
                     : styles.active
                   : ""
               ])}
-              onClick={() => buttonAction(index, button.link)}
+              onClick={() => onChangeView(button.mode)}
             >
               <FontAwesomeIcon icon={button.icon} />
               <span>{button.label}</span>
--- src/pages/CoachDashboard/CoachDashboard.tsx
+++ src/pages/CoachDashboard/CoachDashboard.tsx
@@ -3,14 +3,24 @@ import {useApi} from "../../hooks/useApi.ts";
 import type {CoachDashboardData} from "./index";
 import AverageTeamCard from "../../components/AverageTeamCard/AverageTeamCard.tsx";
 import HeaderDashboard from  "../../components/HeaderDashboard/HeaderDashboard.tsx";
+import {useState} from "react";
+
+type ViewMode = 'team' | 'individual' | 'compare';
 
 const CoachDashboard = () => {
   const { data } = useApi<CoachDashboardData>("coach-dashboard");
+  const [viewMode, setViewMode] = useState<ViewMode>('team');
 
   return (
     <div className={styles.container}>
       <AverageTeamCard cards={data?.averageTeamCards ?? []}/>
-      <HeaderDashboard/>
+      <HeaderDashboard
+        viewMode={viewMode}
+        onChangeView={setViewMode}
+      />
+      {/*{viewMode === 'team' && <TeamAnalysis />}*/}
+      {/*{viewMode === 'individual' && <IndividualAnalysis />}*/}
+      {/*{viewMode === 'compare' && <CompareAnalysis />}*/}
     </div>
   );
 }
