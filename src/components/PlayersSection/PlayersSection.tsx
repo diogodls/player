cf0ae3c70e5bb7c56f59014ce8069c4f696a69c1
guzttx <gugamachado98@hotmail.com>
2026-01-27 15:10:01 -0300

    fix: aninhamento de componentes PlayersSection

--- src/components/PlayersSection/PlayersSection.tsx
+++ src/components/PlayersSection/PlayersSection.tsx
@@ -1,15 +1,29 @@
+import PlayerCard from "../PlayerCard/PlayerCard";
 import styles from "./PlayersSection.module.scss";
+import type {Player} from "../../pages/CoachDashboard/index";
 
 type PlayersSectionProps = {
-    children: React.ReactNode;
+  players: Player[];
 };
 
-const PlayersSection = ({ children }: PlayersSectionProps) => {
+const PlayersSection = ({ players }: PlayersSectionProps) => {
+  if (!players.length) {
     return (
-        <div className={styles.playersSection}>
-            {children}
-        </div>
+      <div className={styles.playersSection}>
+        <span className={styles.emptyState}>
+          Nenhum jogador disponível
+        </span>
+      </div>
     );
+  }
+
+  return (
+    <div className={styles.playersSection}>
+      {players.map((player) => (
+        <PlayerCard key={player.id} player={player} />
+      ))}
+    </div>
+  );
 };
 
 export default PlayersSection;
--- src/pages/CoachDashboard/CoachDashboard.tsx
+++ src/pages/CoachDashboard/CoachDashboard.tsx
@@ -2,7 +2,6 @@ import styles from "./CoachDashboard.module.scss";
 import {useApi} from "../../hooks/useApi.ts";
 import type {CoachDashboardData} from "./index";
 import AverageTeamCard from "../../components/AverageTeamCard/AverageTeamCard.tsx";
-import PlayerCard from "../../components/PlayerCard/PlayerCard.tsx";  
 import HeaderDashboard from  "../../components/HeaderDashboard/HeaderDashboard.tsx";
 import {useState} from "react";
 import PlayersSection from "../../components/PlayersSection/PlayersSection.tsx";
@@ -17,11 +16,8 @@ const CoachDashboard = () => {
     <div className={styles.container}>
       <AverageTeamCard cards={data?.averageTeamCards ?? []}/>
 
-    <PlayersSection>
-      {data?.players?.map(player => (
-        <PlayerCard key={player.id} player={player} />
-      ))}
-    </PlayersSection>
+      <PlayersSection players = {data?.players ?? []} />
+
       <HeaderDashboard
         viewMode={viewMode}
         onChangeView={setViewMode}
