f1724e382745e0ddda15a47398a6ccba04f23db5
jalobler7 <jablobler@inf.ufsm.br>
2026-02-02 11:19:56 -0300

    feat(#30): log de acoes container

--- src/components/ActionLog/ActionLog.tsx
+++ src/components/ActionLog/ActionLog.tsx
@@ -1,9 +1,28 @@
+import styles from "./ActionLog.module.scss"
+
+//fazer: definir tamanho limite da div, ações boas(verde), ruins(vermelha)
+
+
 const ActionLog = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.title}>
+          <span>Linha do tempo de ações</span>
+          <span className={styles.badge}>0</span>
+        </div>
 
+        <div className={styles.actions}>
+          <button className={styles.save}>Salvar</button>
+          <button className={styles.clear}>Limpar</button>
+        </div>
+      </div>
+
+      <div className={styles.emptyState}>
+        <span>Sem ações taggeadas. Comece a taggear ações e elas aparecerão aqui.</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ActionLog
