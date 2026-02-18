8159bc351f6a3b85876dbf1e8e212af25b53a15e
guzttx <gugamachado98@hotmail.com>
2026-02-18 14:44:13 -0300

    feat(#28)-cria header da analise individual

--- src/components/HeaderAnalysis/HeaderAnalysis.tsx
+++ src/components/HeaderAnalysis/HeaderAnalysis.tsx
@@ -1,13 +1,42 @@
 import styles from "./HeaderAnalysis.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFloppyDisk, faRotateLeft } from "@fortawesome/free-solid-svg-icons";
+import { useToast } from "../../contexts/ToastContext";
 
 type Props = {
   onSave?: () => void;
-  onClear?: () => void;
+  onClear?: () => void; // props p adicionar lógica na pág posteriormente
 };
 
 const IndividualAnalysisHeader = ({ onSave, onClear }: Props) => {
+  const toast = useToast();
+
+  const handleSave = () => {
+    try {
+      if (onSave) {
+        onSave();
+        toast.success("Log do jogador salvo com sucesso!");
+      } else {
+        toast.error("Nenhuma ação de salvar foi definida.");
+      }
+    } catch (error) {
+      toast.error("Erro ao salvar o log do jogador.");
+    }
+  };
+
+  const handleClear = () => {
+    try {
+      if (onClear) {
+        onClear();
+        toast.success("Log limpo com sucesso!");
+      } else {
+        toast.error("Nenhuma ação de limpeza foi definida.");
+      }
+    } catch (error) {
+      toast.error("Erro ao limpar o log.");
+    }
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
@@ -17,7 +46,7 @@ const IndividualAnalysisHeader = ({ onSave, onClear }: Props) => {
       <div className={styles.actions}>
         <button
           className={`${styles.button} ${styles.save}`}
-          onClick={onSave}
+          onClick={handleSave}
         >
           <FontAwesomeIcon icon={faFloppyDisk} />
           Salvar log do jogador
@@ -25,7 +54,7 @@ const IndividualAnalysisHeader = ({ onSave, onClear }: Props) => {
 
         <button
           className={`${styles.button} ${styles.clear}`}
-          onClick={onClear}
+          onClick={handleClear}
         >
           <FontAwesomeIcon icon={faRotateLeft} />
           Limpar Log
