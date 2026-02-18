9073cbe1a8ad2600600819c43338fe39565f2083
guzttx <gugamachado98@hotmail.com>
2026-02-18 11:26:22 -0300

    cria header

--- src/pages/IndividualAnalysis/IndividualAnalysis.tsx
+++ src/pages/IndividualAnalysis/IndividualAnalysis.tsx
@@ -1,10 +1,15 @@
 import styles from "../CoachDashboard/CoachDashboard.module.scss";
 import VideoAnalysis from "../../components/VideoAnalysis/VideoAnalysis.tsx";
 import ActionLog from "../../components/ActionLog/ActionLog.tsx";
+import HeaderAnalysis from "../../components/HeaderAnalysis/HeaderAnalysis.tsx";
 
 const IndividualAnalysis = () => {
   return (
     <div className={styles.container}>
+      <HeaderAnalysis
+        onSave={() => console.log("Salvar")}
+        onClear={() => console.log("Limpar")}
+      />
       <VideoAnalysis/>
       <ActionLog/>
     </div>
