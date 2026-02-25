98b326b7e469606edf61cb49c0dd6c4791a7d4f9
jalobler7 <jablobler@inf.ufsm.br>
2026-02-25 11:06:27 -0300

    feat(#48): faz header

--- src/components/SessionAnalysis/SessionAnalysisHeader/SessionAnalysisHeader.tsx
+++ src/components/SessionAnalysis/SessionAnalysisHeader/SessionAnalysisHeader.tsx
@@ -1,16 +1,44 @@
-const SessionAnalysisHeader = () => {
-  return(
-    <div>
-      <div>
-        <span> DETALHES </span>
-      </div>
-      <div>
-        <button>Analise Individual</button>
-        <button>Analise de equipe</button>
-      </div>
+import styles from "./SessionAnalysisHeader.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faUsers } from "@fortawesome/free-solid-svg-icons";
 
-    </div>
+type Tab = "individual" | "team";
+
+type Props = {
+  active?: Tab;
+  onChange?: (tab: Tab) => void;
+};
+
+const SessionAnalysisHeader = ({ active = "individual", onChange }: Props) => {
+  return (
+    <header className={styles.header}>
+      <h2 className={styles.title}>Detalhes</h2>
+
+      <div className={styles.actions}>
+        <button
+          type="button"
+          className={`${styles.pill} ${styles.pillBlue} ${
+            active === "individual" ? styles.active : ""
+          }`}
+          onClick={() => onChange?.("individual")}
+        >
+          <FontAwesomeIcon icon={faUser} className={styles.icon} />
+          Análise Individual
+        </button>
+
+        <button
+          type="button"
+          className={`${styles.pill} ${styles.pillGreen} ${
+            active === "team" ? styles.active : ""
+          }`}
+          onClick={() => onChange?.("team")}
+        >
+          <FontAwesomeIcon icon={faUsers} className={styles.icon} />
+          Análise de Equipe
+        </button>
+      </div>
+    </header>
   );
-}
+};
 
 export default SessionAnalysisHeader;
