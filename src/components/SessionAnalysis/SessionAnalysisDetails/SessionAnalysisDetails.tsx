4d7dfea069617cab5ccb3b784906871be4102dcf
jalobler7 <jablobler@inf.ufsm.br>
2026-02-26 10:09:22 -0300

    feat(#48): cria session details

---
+++ src/components/SessionAnalysis/SessionAnalysisDetails/SessionAnalysisDetails.tsx
@@ -0,0 +1,23 @@
+const SessionAnalysisDetails = () => {
+  return(
+    <div>
+      <div>
+        <span> jogo/treino</span>
+        <span> adversario</span>
+      </div>
+      <div>
+        <div>
+          <span>data</span>
+        </div>
+        <div>
+          local
+        </div>
+        <div>
+          video
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SessionAnalysisDetails;
