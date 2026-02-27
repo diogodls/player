b703b04783deefb7241aa0812a8bf6b313b3a5d9
didi <diogoddlsr@gmail.com>
2026-02-27 12:55:51 -0300

    fix(#28): corrige rebase e tela de analise individual

--- src/pages/IndividualAnalysis/IndividualAnalysis.tsx
+++ src/pages/IndividualAnalysis/IndividualAnalysis.tsx
@@ -1,17 +1,37 @@
-import styles from "../CoachDashboard/CoachDashboard.module.scss";
-import VideoAnalysis from "../../components/VideoAnalysis/VideoAnalysis.tsx";
-import ActionLog from "../../components/ActionLog/ActionLog.tsx";
+import styles from "./IndividualAnalysis.module.scss";
+import VideoAnalysis from "../../components/elements/VideoAnalysis/VideoAnalysis.tsx";
+import ActionLog from "../../components/IndivididualAnalysis/ActionLog/ActionLog.tsx";
+import PlayerSelector from "../../components/IndivididualAnalysis/PlayerSelector/PlayerSelector.tsx";
+import {useApi} from "../../hooks/useApi.ts";
+import {useState} from "react";
+import ActionsModal from "../../components/IndivididualAnalysis/ActionsModal/ActionsModal.tsx";
+import type {IndividualAnalysisData} from "./index";
 import HeaderAnalysis from "../../components/HeaderAnalysis/HeaderAnalysis.tsx";
 
 const IndividualAnalysis = () => {
+  const {data} = useApi<IndividualAnalysisData>("individual-analysis");
+  const [actionsModalOpen, setActionsModalOpen] = useState(false);
+
   return (
     <div className={styles.container}>
-      <HeaderAnalysis
-        onSave={() => console.log("Salvar")}
-        onClear={() => console.log("Limpar")}
-      />
-      <VideoAnalysis/>
-      <ActionLog/>
+      <div className={styles.content}>
+        <HeaderAnalysis
+          onSave={() => console.log("Salvar")}
+          onClear={() => console.log("Limpar")}
+        />
+        <div className={styles.leftContent}>
+          <PlayerSelector players={data?.players ?? []} setActionsModalOpen={setActionsModalOpen}/>
+        </div>
+        <div className={styles.videoAnalysis}>
+          <VideoAnalysis/>
+        </div>
+        <div className={styles.actionLog}>
+          <ActionLog/>
+        </div>
+      </div>
+      {actionsModalOpen && <ActionsModal
+        closeModal={() => setActionsModalOpen(false)}
+        actions={data?.actions ?? []}/>}
     </div>
   );
 };
