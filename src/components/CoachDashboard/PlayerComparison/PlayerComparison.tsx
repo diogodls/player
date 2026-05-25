7ca92d3eea44e627859d4d7da0e08ebd7288bd36
jalobler7 <jablobler@inf.ufsm.br>
2026-05-25 16:23:42 -0300

    fix(#36) : corrige erros de largura e espacamento

--- src/components/CoachDashboard/PlayerComparison/PlayerComparison.tsx
+++ src/components/CoachDashboard/PlayerComparison/PlayerComparison.tsx
@@ -113,7 +113,7 @@ const PlayerComparison = ({ players, metrics }: PlayerComparisonProps) => {
       }
 
       {selectedPlayers.filter(Boolean).length > 1 &&
-        <div>
+        <div className={styles.comparisonResults}>
           <PlayerRadarChart players={selectedPlayers} showButtons metrics={metrics ?? []} />
           <ComparativePlayerInfos selectedPlayers={selectedPlayers} metrics={metrics} />
         </div>
